969e92b83e2ac596b9f40eedbd81201280d183e1
sedagormus89 <sedagormus89@hotmail.com>
2016-02-20 22:54:06 -0800

    Added "Shout It" files

---
+++ process.php
@@ -0,0 +1,32 @@
+<?php
+include 'database.php';
+
+//Check if form is submitted
+
+if(isset($_POST['submit'])) {
+	$user = mysqli_real_escape_string($con, $_POST['user']);
+	$message = mysqli_real_escape_string($con, $_POST['message']);
+	
+	//Set timezone 
+	date_default_timezone_set('America/Los_Angeles');
+	$time = date('h:i:s a',time());
+	
+	//Validate input
+if (!isset($user) || $user== '' || !isset($message)) {
+		$error = "Please fill in your name and message";
+		header("Location: index.php?error=".urlencode($error));
+		exit();
+	}	
+	else{
+		$query = "INSERT INTO shouts (user,message,time)
+			VALUES('$user','$message','$time')";
+		if(!mysqli_query($con, $query)) {
+			die('Error: '.mysqli_error($con));
+		}
+		else {
+			header("Location: index.php");
+			exit();
+		}
+	}
+}
+?>
